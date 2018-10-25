c1b0b75f8c1961a4c7e8d3eaaee54ed8f7409b9f
candaceclaireee <mercadocandaceclaire@gmail.com>
2018-10-25 22:19:05 +0800

    Resolved LB 03 - Back Button

--- app.js
+++ app.js
@@ -38,6 +38,11 @@ app.use(session({
     saveUninitialized :true,
   }))
 
+app.use(function(req, res, next) {
+    res.set('Cache-Control', 'no-cache, private, no-store, must-revalidate, max-stale=0, post-check=0, pre-check=0');
+    next()
+});
+  
 app.use(require("./controllers"))
 
 app.listen(3000, () => {
