9593be9cd465ebbaabd0f42fbcfa35833d953375
sairakaye <skcmanalili@gmail.com>
2018-11-25 23:48:19 +0800

    Admin can change the status.

--- public/js/admin-view.js
+++ public/js/admin-view.js
@@ -30,6 +30,34 @@ $("#remarks-form").submit(function (e) {
     },
     success: function() {
       console.log("Grant status changed!")
+
+      var promptDiv = document.getElementById("prompt")
+      $(promptDiv).empty()
+
+      var messageDiv = document.createElement("div")
+
+      $(messageDiv).addClass("ui positive message transition hidden fluid")
+
+      var icon = document.createElement("i")
+      $(icon).addClass("close icon")
+
+      var headerDiv = document.createElement("div")
+      $(headerDiv).addClass("header")
+      $(headerDiv).append("Grant Status Changed!")
+
+      var paragraph = document.createElement("p")
+      $(paragraph).append("You successfully changed the status! :D")
+
+
+      $(messageDiv).append(icon)
+      $(messageDiv).append(headerDiv)
+      $(messageDiv).append(paragraph)
+      $(promptDiv).append(messageDiv)
+
+      $('.message .close').on('click', function() { 
+        $(this).closest('.message')
+        .transition('fade')
+      });
     },
     error: function() {
       alert("Something went wrong!")
