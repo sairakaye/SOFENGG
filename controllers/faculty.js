ad8fda9a3207f1596a9b383b5770de9328dcb8e3
Candace Claire Mercado <mercadocandaceclaire@gmail.com>
2018-11-27 22:22:49 +0800

    Refined LB2 13 By Using User ID

--- controllers/faculty.js
+++ controllers/faculty.js
@@ -1259,19 +1259,48 @@ module.exports = router
  * @param {options} options.fn
  * @param {options} options.inverse
  */
-hbs.registerHelper('showonlybyuser', function (fname, lname, options) {
+hbs.registerHelper('showonlybyuser', function (id, options) {
 
   var user = controllerUser.getCurrentUser()
-  var firstName = user.firstName
-  var lastName = user.lastName
+  var formOnes = user.fdOneForms
+  var formTwos = user.fdTwoForms
+  var formThrees = user.fdThreeForms
+  var formFours = user.fdFourForms
+  var formFifteens = user.fdFifteenForms
+  var formSixteens = user.fdSixteenForms
 
   if (user != undefined || user != null) {
-    if (firstName + "" == fname + "") {
-      if (lastName + "" == lname + "")
+    for (var i = 0; i < formOnes.length; i++) {
+      if (id.toString() == formOnes[i]._id.toString()){
         return options.fn(this);
-    } else {
-      return options.inverse(this);
+      }
     }
+    for (var i = 0; i < formTwos.length; i++) {
+      if (id.toString()  == formTwos[i]._id.toString() ){
+        return options.fn(this);
+      }
+    }
+    for (var i = 0; i < formThrees.length; i++) {
+      if (id.toString()  == formThrees[i]._id.toString() ){
+        return options.fn(this);
+      }
+    }
+    for (var i = 0; i < formFours.length; i++) {
+      if (id.toString()  == formFours[i]._id.toString() ){
+        return options.fn(this);
+      }
+    }
+    for (var i = 0; i < formFifteens.length; i++) {
+      if (id.toString()  == formFifteens[i]._id.toString() ){
+        return options.fn(this);
+      }
+    }
+    for (var i = 0; i < formSixteens.length; i++) {
+      if (id.toString()  == formSixteens[i]._id.toString() ){
+        return options.fn(this);
+      }
+    }
+    return options.inverse(this);
   }
 })
 
