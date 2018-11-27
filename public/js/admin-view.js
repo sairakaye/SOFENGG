f9051c570d52a3641a6a903631eb5704346a1238
sairakaye <skcmanalili@gmail.com>
2018-11-28 04:41:44 +0800

    FIxed deleting of remarks

--- public/js/admin-view.js
+++ public/js/admin-view.js
@@ -15,43 +15,48 @@ $('.ui.dropdown').dropdown();
 var formID = $("#submit-button").attr("data-id")
 var grant = $("#submit-button").attr("data-grant")
 var currentStatus = $("#form-status").val()
+var $tr
+var id
 
-$("button#delete-button").on("click", function () {
-  var $tr = $(this).closest('tr')
-  var id = $(this).attr("data-id")
+$("button#delete-button").on("click", function (e) {
+  $tr = $(this).closest('tr')
+  id = $(this).attr("data-id")
 
   $('.ui.modal.deletion')
     .modal('show');
+})
 
-  $(".negative.delete.final").on("click", function () {
-    $.ajax({
-      method: "delete",
-      url: "delete-remark",
-      data: {
-        id,
-        formID,
-        grant
-      },
-      success: function (result) {
-        /**
-         * This callback calls a lot of times. Will fix this later.
-         */
+$(".negative.delete.final").on("click", function () {
+  $.ajax({
+    method: "delete",
+    url: "delete-remark",
+    data: {
+      id,
+      formID,
+      grant
+    },
+    success: function (result) {
+      if (result.n == 1) {
         $tr.remove();
-      }
-    })
+        $tr = null
+        id = null
+      } else
+        alert("Something went wrong!")
+    }
   })
 })
 
-$("button#reset-button").on("click", function() {
+
+$("#reset-button").on("click", function() {
   $(".dropdown").dropdown('restore defaults');
-  $("#status").val("")
+  $("#status-dropdown").val("")
   $("#remark").val("")
 })
 
 $("#remarks-form").submit(function (e) {
   e.preventDefault();
 
-  var status = $("#status").val()
+  var status = $("#status-dropdown").val()
   var remark = $("#remark").val()
 
   if (remark == "" && status == "") {
@@ -136,13 +141,15 @@ $("#remarks-form").submit(function (e) {
 
         var paragraph = document.createElement("p")
         if (status != "" && remark == "") {
+          $("#form-status").val(status)
+          currentStatus = $("#form-status").val()
           $(paragraph).append("You successfully changed the status of " + grant + " to " + status + ".")
-          currentStatus = $("#form-status").val(status)
         } else if (status == "" && remark != "") {
           $(paragraph).append("You successfully added the remark in " + grant + ".")
         } else if (status != "" || remark != "") {
+          $("#form-status").val(status)
+          currentStatus = $("#form-status").val()
           $(paragraph).append("You successfully changed the status of " + grant + " to " + status + " and added the remark.")
-          currentStatus = $("#form-status").val(status)
         }
 
         $(messageDiv).append(icon)
@@ -163,37 +170,11 @@ $("#remarks-form").submit(function (e) {
           var tdRemark = document.createElement("td")
           var tdButton = document.createElement("td")
           var deleteButton = document.createElement("button")
-
+          
           $(deleteButton).addClass("ui button red")
           $(deleteButton).attr("data-id", result._id)
-          $(deleteButton).attr("id", "new-delete-button")
+          $(deleteButton).attr("id", "delete-button")
           $(deleteButton).append("Delete")
-
-          $(document).on('click', "#new-delete-button", function () {
-            var $tr = $(this).closest('tr')
-            var id = $(this).attr("data-id")
-
-            $('.ui.modal.deletion')
-              .modal('show');
-
-            $(".negative.delete.final").on("click", function () {
-              $.ajax({
-                method: "delete",
-                url: "delete-remark",
-                data: {
-                  id,
-                  formID,
-                  grant
-                },
-                success: function (result) {
-                  /**
-                    * This callback calls a lot of times. Will fix this later.
-                    */
-                  $tr.remove();
-                }
-              })
-            })
-          })
 
           $(tdDate).append(moment(result.date).format("MMMM D, YYYY h:mm A"))
           $(tdStatus).append(result.status)
@@ -207,9 +188,17 @@ $("#remarks-form").submit(function (e) {
 
           $(tbodyElement).append(trElement)
 
+          $(document).on("click", "#delete-button", function() {
+            $tr = $(this).closest('tr')
+            id = $(this).attr("data-id")
+          
+            $('.ui.modal.deletion')
+              .modal('show');         
+          })
+
           $(".dropdown").dropdown('restore defaults');
-          var status = $("#status").val("")
-          var remark = $("#remark").val("")
+          $("#status-dropdown").val("")
+          $("#remark").val("")
         }
       },
 
